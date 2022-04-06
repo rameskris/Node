a920e94aee8e355feabea4bee9e69ecd6e4650da
rameskris <redmiblack2@gmail.com>
2022-04-07 01:15:07 +0530

    readfileandsyncfile completed

--- 7_fsmoduleAsync.js
+++ 7_fsmoduleAsync.js
@@ -1,23 +1,23 @@
 const {readFile,writeFile}=require('fs')
+
 readFile('./pathfol/first.txt','utf-8',(err,result)=>{
     if(err){
-        console.log(err);
-        return
+        console.log(err)
     }
-    const first=result
+   const first=result
+   readFile('./pathfol/second.txt','utf-8',(err,result)=>{
+       if(err){
+        console.log(err)
+    }
+   const second=result
+   writeFile('./pathfol/newFile.txt',`Here is the result :${first},${second}`,(err,result)=>{
+       if(err){
+        console.log(err)
+    }
     console.log(result)
-    readFile('./pathfol/second.txt','utf-8',(err,result)=>{
-        if(err){
-            console.log(err);
-            return
-        }
-        const second=result
-        writeFile('./pathfol/newFileagain.txt',`${first} and ${second}`,{flag:'a'},(err,res)=>{
-           console.log(res)
-        })
+       
 
-    })
-    
-   
+   })
+
+   })
 })
-
